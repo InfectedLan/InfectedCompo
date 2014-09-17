2f054eed9dd7f3a2d4078b3579f4eb4fe2eb4a4e
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-09-17 23:20:55 +0200

    More team stuff

--- v2/pages/addTeam.php
+++ v2/pages/addTeam.php
@@ -1,7 +1,18 @@
 <?php
 class PageContent {
 	public function render() {
-		echo "Team.";
+		echo "<h1>Lag team</h1>";
+		echo "<table>";
+			echo '<tr>';
+				echo '<td>';
+					echo '<h3>Et par detaljer...</h3>';
+					echo '<p>Før du kan lage klanen må vi få vite litt mer om den. Vennligst fyll ut litt mer om den...</p>';
+				echo '</td>';
+				echo '<td>';
+					echo '<h3>Inviter teammates</h3>';
+				echo '</td>';
+			echo '</tr>';
+		echo "</table>";
 	}
 }
 ?>
