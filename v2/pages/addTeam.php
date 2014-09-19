ec904ea6ad67cad873d4577cf92c2a172fa5a4c3
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-09-20 01:46:36 +0200

    team stuff

--- v2/pages/addTeam.php
+++ v2/pages/addTeam.php
@@ -1,15 +1,63 @@
 <?php
+require_once 'handlers/compohandler.php';
+require_once 'handlers/eventhandler.php';
 class PageContent {
 	public function render() {
+		$currEvent = EventHandler::getCurrentEvent();
 		echo "<h1>Lag team</h1>";
 		echo "<table>";
 			echo '<tr>';
-				echo '<td>';
+				echo '<td width="50%">';
 					echo '<h3>Et par detaljer...</h3>';
-					echo '<p>Før du kan lage klanen må vi få vite litt mer om den. Vennligst fyll ut litt mer om den...</p>';
+					echo '<p>Før du kan lage klanen må vi få vite litt mer om den. Vennligst fyll ut litt mer om den. Du kan for øyeblikket bare melde en klan på en compo. Lag flere dersom dere skal delta i flere kompoer.</p>';
+					echo '<table>';
+						echo '<tr>';
+							echo '<td>';
+								echo 'Navn:';
+							echo '</td>';
+							echo '<td>';
+								echo '<input type="text" id="clanName" />';
+							echo '</td>';
+						echo '</tr>';
+						echo '<tr>';
+							echo '<td>';
+								echo 'Tag:';
+							echo '</td>';
+							echo '<td>';
+								echo '<input type="text" id="clanTag" />';
+							echo '</td>';
+						echo '</tr>';
+						echo '<tr>';
+							echo '<td>';
+								echo 'Compo:';
+							echo '</td>';
+							echo '<td>';
+								echo '<select id="compoSelect">';
+									$compos = CompoHandler::getComposForEvent($currEvent);
+									foreach ($compo as $compos) {
+										echo '<option value="' . $compo->getId() . '">' . $compo->getName() . '</option>';
+									}
+								echo '</select>';
+							echo '</td>';
+						echo '</tr>';
+						echo '<tr>';
+							echo '<td>';
+								echo '<input type="button" value="Lag klan!" onClick="registerClan()" />';
+							echo '</td>';
+						echo '</tr>';
+					echo '</table>';
 				echo '</td>';
-				echo '<td>';
+				echo '<td width="50%">';
 					echo '<h3>Inviter teammates</h3>';
+					echo 'Søk: <input id="inviteSearchBox" type="text" />';
+					echo '<br />';
+					echo '<div id="searchResultsResultPane">';
+
+					echo '</div>';
+					echo '<h3>Inviterte folk:</h3>';
+					echo '<div id="invidedPlayers">';
+
+					echo '</div>';
 				echo '</td>';
 			echo '</tr>';
 		echo "</table>";
