ae7da3d9d3caba536105c78dc668fe7176e1ae76
Liam Svanåsbakken Crouch <spam@petterroea.com>
2015-08-25 17:40:57 +0200

    Disabled chat for now

--- v2/site.php
+++ v2/site.php
@@ -40,12 +40,12 @@ class Site {
 
 				//Custom javascripts. This HAS to be included after jquery
 				echo '<script src="scripts/shared.js"></script>';
-
+                /*
                 if(Session::isAuthenticated()) {
                     echo '<script src="../api/scripts/chat.js"></script>';
                     echo '<script>Chat.init(); $(document).ready(function() {Chat.bindChat("chatContainer", 1, 415);});</script>';
                 }
-
+                */
 				echo "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-54254513-4', 'auto');ga('send', 'pageview');</script>";
 			echo '</head>';
 
