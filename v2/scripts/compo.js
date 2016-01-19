4afd3e71fe39dd968433db507ccaf1d35336a816
Liam Svanåsbakken Crouch <spam@petterroea.com>
2016-01-19 17:30:00 +0100

    fixed small bug

--- v2/scripts/compo.js
+++ v2/scripts/compo.js
@@ -31,10 +31,10 @@ $(document).ready(function() {
 	compoStatusUpdateId = setInterval(updateCompoStatus, 15000);
 	updateCompoStatus();
 	$("#gameBannerCsGo").click(function(e) {
-		window.location = "index.php?page=compo&id=5";
+		window.location = "index.php?page=compo&id=7";
 	});
 	$("#gameBannerLoL").click(function(e) {
-		window.location = "index.php?page=compo&id=6";
+		window.location = "index.php?page=compo&id=8";
 	});
 	$("#gameBannerCurrentMatch").click(function(e) {
 		window.location = "index.php?page=match";
